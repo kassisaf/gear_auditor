196114e795057174fdc553ecd2f3671b8c4a1961
Andrew Kassis <kassisaf@gmail.com>
2024-01-29 15:16:43 -0800

    Improve gearswap parsing, handle augmented items

--- modules/gearswap.py
+++ modules/gearswap.py
@@ -23,9 +23,9 @@ EQUIPMENT_SLOTS = [
     'neck',
     'waist',
 ]
-AUGMENTED_ITEM_RE = re.compile(r'{\s*(name)=\s*"(.+)",\s*augments\s*=')
+AUGMENTED_ITEM_RE = re.compile(r'(\S+) *= *{\s*name *=(.+),\s*augments *= *{')
 JOB_ABBREVIATION_RE = re.compile(r'^[A-Z]{3}$')
-GEAR_SLOT_RE = re.compile(f"({'|'.join([f'({slot})' for slot in EQUIPMENT_SLOTS])})" + r"\s*=\s*")
+GEAR_SLOT_RE = re.compile(f"({'|'.join([f'(?:{slot})' for slot in EQUIPMENT_SLOTS])})" + r" *= *([^,\r\n]+),?")
 
 
 class GearSwapLuaFile:
@@ -79,20 +79,31 @@ class GearSwapLuaFile:
     def _parse_equipment_and_variables(self):
         with open(self._file_path, 'r', encoding='utf8') as file:
             for line in file:
-                if line.strip().startswith(LUA_COMMENT_OPERATOR):
+                line = line.strip()
+                # Ignore comments
+                if line.startswith(LUA_COMMENT_OPERATOR):
                     continue
 
-                if GEAR_SLOT_RE.search(line):
-                    terms = [term.strip().replace(',', '') for term in line.split('=')]
-                    if len(terms) >= 2 and terms[1] != EMPTY_SLOT:
-                        terms[1] = terms[1].split(LUA_COMMENT_OPERATOR)[0].strip()  # remove trailing comments
-                        if is_quoted(terms[1]):
-                            unquoted = remove_surrounding_quotes(terms[1])
-                            if len(terms[1]) > len(unquoted):
-                                self._equipment_names.append(unquoted.lower())
-                        else:
-                            # TODO: Attempt to look up variable's item name
-                            # if AUGMENTED_ITEM_PATTERN.search(line):
-                            self._equipment_variables.append(terms[1])
+                # Handle lines following the pattern: slot = item
+                gear_slot_search = GEAR_SLOT_RE.search(line)
+                if gear_slot_search is not None:
+                    slot, item_name = gear_slot_search.groups()
+                    if item_name == EMPTY_SLOT:
+                        continue
+                    self._add_equipment(item_name)
+                    continue
+
+                # Handle lines defining an augmented item
+                augmented_item_search = AUGMENTED_ITEM_RE.search(line)
+                if augmented_item_search is not None:
+                    item_variable, item_name = augmented_item_search.groups()
+                    print(f'found augmented item: {item_variable} = {item_name}')
+                    self._add_equipment(item_name)
+                    continue
+
         self._equipment_names = list(set(self._equipment_names))
         self._equipment_variables = list(set(self._equipment_variables))
+
+    def _add_equipment(self, item_name: str):
+        unquoted_item_name = remove_surrounding_quotes(item_name)
+        self._equipment_names.append(unquoted_item_name.lower())
